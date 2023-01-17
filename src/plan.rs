7e53d8aaadfbff48f686a4cd73b915ca8494ab45
Roberto Ariosa <roberto.ariosa@upc.edu>
2023-01-17 12:32:01 +0100

    Formatting

--- src/plan.rs
+++ src/plan.rs
@@ -1,69 +1,63 @@
 use std::fmt::Display;
 
-use crate::tokens::parse_id;
-use nom::{
-	character::complete::line_ending,
-	multi::many0,
-	sequence::{delimited, terminated},
-	IResult,
-};
-use nom::{
-	character::complete::{alphanumeric0, char, space1},
-	combinator::map,
-	multi::separated_list0,
-	sequence::separated_pair,
-};
+use nom::character::complete::{alphanumeric0, char, line_ending, space1};
+use nom::combinator::map;
+use nom::multi::{many0, separated_list0};
+use nom::sequence::{delimited, separated_pair, terminated};
+use nom::IResult;
 use serde::{Deserialize, Serialize};
+
+use crate::tokens::parse_id;
 
 pub type Parameter = String;
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
 pub struct Action {
-	pub name: String,
-	#[serde(default)]
-	pub parameters: Vec<Parameter>,
+    pub name: String,
+    #[serde(default)]
+    pub parameters: Vec<Parameter>,
 }
 
 impl Action {
-	fn parse(input: &str) -> IResult<&str, Self> {
-		println!("parse: {}", input);
-		let (output, (name, parameters)) = delimited(
-			char('('),
-			separated_pair(Action::parse_name, space1, Action::parse_parameters),
-			char(')'),
-		)(input)?;
-		Ok((output, Action { name, parameters }))
-	}
+    fn parse(input: &str) -> IResult<&str, Self> {
+        println!("parse: {}", input);
+        let (output, (name, parameters)) = delimited(
+            char('('),
+            separated_pair(Action::parse_name, space1, Action::parse_parameters),
+            char(')'),
+        )(input)?;
+        Ok((output, Action { name, parameters }))
+    }
 
-	fn parse_name(input: &str) -> IResult<&str, String> {
-		println!("parse_name: {}", input);
-		let (output, name) = parse_id(input)?;
-		Ok((output, name))
-	}
+    fn parse_name(input: &str) -> IResult<&str, String> {
+        println!("parse_name: {}", input);
+        let (output, name) = parse_id(input)?;
+        Ok((output, name))
+    }
 
-	fn parse_parameters(input: &str) -> IResult<&str, Vec<Parameter>> {
-		println!("parse_parameters: {}", input);
-		let (output, parameters) = separated_list0(space1, map(alphanumeric0, String::from))(input)?;
-		Ok((output, parameters))
-	}
+    fn parse_parameters(input: &str) -> IResult<&str, Vec<Parameter>> {
+        println!("parse_parameters: {}", input);
+        let (output, parameters) = separated_list0(space1, map(alphanumeric0, String::from))(input)?;
+        Ok((output, parameters))
+    }
 }
 
 impl Display for Action {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}({})", self.name, self.parameters.join(", "))
-	}
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}({})", self.name, self.parameters.join(", "))
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
 pub struct Plan(pub Vec<Action>);
 
 impl Plan {
-	pub fn parse(input: &str) -> IResult<&str, Self> {
-		let (output, plan) = many0(terminated(Action::parse, line_ending))(input).unwrap();
-		Ok((output, Plan(plan)))
-	}
+    pub fn parse(input: &str) -> IResult<&str, Self> {
+        let (output, plan) = many0(terminated(Action::parse, line_ending))(input).unwrap();
+        Ok((output, Plan(plan)))
+    }
 
-	pub fn actions(&self) -> impl Iterator<Item = &Action> {
-		self.0.iter()
-	}
+    pub fn actions(&self) -> impl Iterator<Item = &Action> {
+        self.0.iter()
+    }
 }
--- src/tokens.rs
+++ src/tokens.rs
@@ -1,8 +1,10 @@
+use nom::bytes::complete::take_while;
+use nom::character::is_alphanumeric;
 use nom::combinator::map;
-use nom::{bytes::complete::take_while, character::is_alphanumeric, IResult};
+use nom::IResult;
 
 pub fn parse_id(input: &str) -> IResult<&str, String> {
-	println!("parse_id: {}", input);
-	let (output, id) = map(take_while(|c: char| is_alphanumeric(c as u8) || c == '-'), String::from)(input)?;
-	Ok((output, id))
+    println!("parse_id: {}", input);
+    let (output, id) = map(take_while(|c: char| is_alphanumeric(c as u8) || c == '-'), String::from)(input)?;
+    Ok((output, id))
 }
