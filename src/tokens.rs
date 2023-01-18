70023706179fa2c04355a09e44065c0266119306
Roberto Ariosa <roberto.ariosa@upc.edu>
2023-01-18 17:00:58 +0100

    Treat comments as whitespace

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,12 +1,15 @@
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take_while};
-use nom::character::complete::{alpha1, alphanumeric1, char, multispace0};
-use nom::character::is_alphanumeric;
+use nom::character::complete::{alpha1, alphanumeric1, char, multispace0, not_line_ending};
 use nom::combinator::{map, recognize};
 use nom::error::ParseError;
 use nom::multi::many0_count;
 use nom::sequence::{delimited, pair, preceded};
 use nom::IResult;
+use nom::{
+    character::{complete::line_ending, is_alphanumeric},
+    combinator::opt,
+};
 
 pub fn parse_id(input: &str) -> IResult<&str, String> {
     let (output, id) = map(take_while(|c: char| is_alphanumeric(c as u8) || c == '-'), String::from)(input)?;
@@ -17,7 +20,15 @@ pub fn ws<'a, F, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> 
 where
     F: FnMut(&'a str) -> IResult<&'a str, O, E>,
 {
-    delimited(multispace0, inner, multispace0)
+    delimited(
+        multispace0,
+        delimited(
+            opt(delimited(multispace0, comment, multispace0)),
+            inner,
+            opt(delimited(multispace0, comment, multispace0)),
+        ),
+        multispace0,
+    )
 }
 
 pub fn id(i: &str) -> IResult<&str, String> {
@@ -31,4 +42,8 @@ pub fn id(i: &str) -> IResult<&str, String> {
 pub fn var(i: &str) -> IResult<&str, String> {
     let (input, identifier) = preceded(char('?'), id)(i)?;
     Ok((input, identifier))
+}
+
+pub fn comment<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &str, E> {
+    delimited(tag(";"), not_line_ending, line_ending)(i)
 }
