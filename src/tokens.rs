a09c131187f154b46e670aa39b546e4cf5b2801e
Roberto Ariosa <roberto.ariosa@upc.edu>
2023-02-20 12:21:55 +0100

    Add number parsing

--- src/tokens.rs
+++ src/tokens.rs
@@ -16,3 +16,17 @@ pub fn var(i: TokenStream) -> IResult<TokenStream, String, ParserError> {
         _ => Err(nom::Err::Error(ParserError::ExpectedIdentifier)),
     }
 }
+
+pub fn float(i: TokenStream) -> IResult<TokenStream, f64, ParserError> {
+    match i.peek() {
+        Some((Token::Float(s), _)) => Ok((i.advance(), s)),
+        _ => Err(nom::Err::Error(ParserError::ExpectedFloat)),
+    }
+}
+
+pub fn integer(i: TokenStream) -> IResult<TokenStream, i64, ParserError> {
+    match i.peek() {
+        Some((Token::Integer(s), _)) => Ok((i.advance(), s)),
+        _ => Err(nom::Err::Error(ParserError::ExpectedInteger)),
+    }
+}
